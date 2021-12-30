fdd1a45bcc775f60abdc7c2ed373c7a4081fe236
Abdelrahman A. Abdelrahman <18036@stemegypt.edu.eg>
2021-12-30 21:46:04 +0200

    __________>

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDTmIqZ3mtV9IgV2rh1kI40Ma_iW2fdyls",
+    authDomain: "youth-ministry-a0390.firebaseapp.com",
+    databaseURL: "https://youth-ministry-a0390-default-rtdb.firebaseio.com",
+    projectId: "youth-ministry-a0390",
+    storageBucket: "",
+    messagingSenderId: "958919237109",
+    appId: "1:958919237109:android:ff5c0740ed66da851214bc"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
